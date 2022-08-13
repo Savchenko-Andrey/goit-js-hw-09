290f87cfd6b06bb44711a599fd2bcabfe519c36e
Savchenko-Andrey <95830161+Savchenko-Andrey@users.noreply.github.com>
2022-08-13 17:05:25 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,7 +2,7 @@ import flatpickr from 'flatpickr';
 import "flatpickr/dist/flatpickr.min.css";
 import Notiflix from 'notiflix';
 
-refs = {
+const refs = {
     input: document.querySelector('#datetime-picker'),
     start: document.querySelector('button[data-start]'),
 
